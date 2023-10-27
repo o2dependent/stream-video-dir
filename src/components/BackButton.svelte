0539a095907962e5fc640dfaed905459b4644533
Ethan Olsen <131eolsen@gmail.com>
2023-10-27 12:03:39 -0700

    feat: directory navigation

---
+++ src/components/BackButton.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	export let filepath: string;
+</script>
+
+<div>
+	{#if filepath !== ""}
+		<a
+			href={filepath.split("/").length === 1
+				? "/"
+				: filepath.split("/").slice(0, -1).join("/")}>Back</a
+		>
+	{/if}
+</div>
+
+<style>
+	div {
+		margin: auto;
+		padding: 1rem;
+		width: 800px;
+		max-width: calc(100% - 2rem);
+		color: white;
+		font-size: 20px;
+		line-height: 1.6;
+	}
+</style>
---
+++ src/components/DirectoryList.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	export let directory: Array<string> = [];
+	export let filepath = "";
+	console.log(filepath.split("/").length);
+</script>
+
+<div class="directory__list__container">
+	{#each directory as item}
+		{#if item.endsWith(".mp4")}
+			<a href="{filepath}/{item.substring(0, item.length - 4)}/watch">{item}</a>
+		{:else}
+			<a href="{filepath}/{item}">{item}</a>
+		{/if}
+	{:else}
+		<p>Nothing here.</p>
+	{/each}
+</div>
+
+<style>
+	.directory__list__container {
+		display: flex;
+		flex-direction: column;
+		gap: 0.25rem;
+	}
+</style>
