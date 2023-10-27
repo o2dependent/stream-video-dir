24bf3f6dc7a5e7fce6655751551c837a7be8c2cc
Ethan Olsen <131eolsen@gmail.com>
2023-10-27 16:28:41 -0700

    feat: add node

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
 import { defineConfig } from "astro/config";
 import svelte from "@astrojs/svelte";
-
 import tailwind from "@astrojs/tailwind";
+
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  integrations: [svelte(), tailwind()]
+  integrations: [svelte(), tailwind()],
+  adapter: node({
+    mode: "standalone"
+  })
 });
