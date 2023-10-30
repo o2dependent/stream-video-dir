c19f7ff374a2a38fe975816707201b68a75ea29e
Ethan Olsen <131eolsen@gmail.com>
2023-10-30 03:15:41 -0700

    chore: add pocketbase to ws server

--- apps/ws/index.ts
+++ apps/ws/index.ts
@@ -2,9 +2,11 @@ import { Server } from "socket.io";
 import os from "os";
 import fs from "fs";
 import cookie from "cookie";
+import PocketBase from "pocketbase";
 // hostname ip
 const hostname = os?.networkInterfaces?.()?.en1?.[1]?.address ?? "";
 export const BASE_VOLUME_PATH = "/Volumes/Extreme SSD/One Pace";
+const pb = new PocketBase("http://127.0.0.1:8090");
 
 const devices: Record<string, any> = {};
 
@@ -20,21 +22,22 @@ io.on("connection", (socket) => {
 	console.log("------------ connection ------------");
 
 	const cookies = socket.request.headers.cookie;
-	const device_id = cookie.parse(cookies ?? "");
+	const { device_id, profile } = cookie.parse(cookies ?? "");
 	console.log({ device_id });
-	socket.on("timeupdate", (data) => {
-		let { filepath, time } = data;
-		// get file or create a new one
-		const filepathArr = filepath.split("/");
-		const videoName = filepathArr.pop();
-		filepath = filepathArr.join("/");
-		const videoDataPath = `${BASE_VOLUME_PATH}/${filepath}/._meta_${videoName}.mp4.json`;
-		fs.writeFileSync(
-			videoDataPath,
-			JSON.stringify({ time, updatedAt: Date.now() }),
-		);
-		socket.emit("timeupdated", { success: true });
-	});
+	!!profile &&
+		socket.on("timeupdate", (data) => {
+			let { filepath, time } = data;
+			// get file or create a new one
+			const filepathArr = filepath.split("/");
+			const videoName = filepathArr.pop();
+			filepath = filepathArr.join("/");
+			const videoDataPath = `${BASE_VOLUME_PATH}/${filepath}/._meta_${videoName}.mp4.json`;
+			fs.writeFileSync(
+				videoDataPath,
+				JSON.stringify({ time, updatedAt: Date.now() }),
+			);
+			socket.emit("timeupdated", { success: true });
+		});
 
 	socket.on("join", (deviceName) => {
 		console.log("join");
