1139733c0c1f49d5d294ae663ac0c2a2baf7c47c
Ethan Olsen <131eolsen@gmail.com>
2023-11-16 08:34:54 -0800

    fix: ws ip

--- apps/ws/index.ts
+++ apps/ws/index.ts
@@ -3,15 +3,17 @@ import os from "os";
 import fs from "fs";
 import cookie from "cookie";
 import PocketBase, { RecordModel } from "pocketbase";
-// hostname ip
-const hostname = os?.networkInterfaces?.()?.en1?.[1]?.address ?? "";
+// get network ip
+export const ip = Object.values(os.networkInterfaces())
+	.flat()
+	.find((iface) => iface?.family === "IPv4" && !iface.internal)?.address;
 export const BASE_VOLUME_PATH = "/Volumes/Extreme SSD/One Pace";
 
 const devices: Record<string, any> = {};
-
+console.log(ip);
 const io = new Server(5432, {
 	cors: {
-		origin: `http://${hostname}:4321`,
+		origin: `http://${ip}:4321`,
 		methods: ["GET", "POST"],
 		credentials: true,
 	},
