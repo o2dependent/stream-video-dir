7b5543af49933ce24e5e048329055c67ce7f7246
Ethan Olsen <131eolsen@gmail.com>
2023-11-27 02:29:28 -0800

    chore: change constant directory to match new architecture

--- apps/web/src/lib/constants.ts
+++ apps/web/src/lib/constants.ts
@@ -1,5 +1,5 @@
 export const TMP_FOLDER = `/tmp/${import.meta.env.APP_NAME || "lowky-video"}`;
-export const BASE_VOLUME_PATH = "/Volumes/Extreme SSD/One Pace";
+export const BASE_VOLUME_PATH = "/Volumes/Extreme SSD";
 export const SCRUB_MAX_CHUNK_INDEX = 10;
 export const SCRUB_SCREENSHOT_WIDTH = 320;
 export const SCRUB_SCREENSHOT_HEIGHT = 180;
