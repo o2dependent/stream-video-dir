234886f31a43e17054591124764386ba0d8a764b
Ethan Olsen <131eolsen@gmail.com>
2023-12-03 15:26:30 -0800

    chore: remove logs

--- apps/web/src/components/listeners/NavigationListener.svelte
+++ apps/web/src/components/listeners/NavigationListener.svelte
@@ -5,23 +5,18 @@
 	const beforepreparation = () => {
 		if (progressTimeout) clearTimeout(progressTimeout);
 		$navProgress = 15;
-		console.log("$navProgress = 15;");
 	};
 	const beforeswap = () => {
 		$navProgress = 75;
-		console.log("$navProgress = 75;");
 	};
 	const afterswap = () => {
 		$navProgress = 90;
-		console.log("$navProgress = 90;");
 	};
 	const pageload = () => {
 		$navProgress = 100;
-		console.log("$navProgress = 100;");
 		if (progressTimeout) clearTimeout(progressTimeout);
 		progressTimeout = setTimeout(() => {
 			$navProgress = 0;
-			console.log("$navProgress = 0;");
 		}, 500);
 	};
 
