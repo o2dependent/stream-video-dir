bfe8d880b5e58cbcdbd137964856969c4e8d2165
Ethan Olsen <131eolsen@gmail.com>
2023-12-06 16:02:35 -0800

    fix: nav progress listener

--- apps/web/src/components/listeners/NavigationListener.svelte
+++ apps/web/src/components/listeners/NavigationListener.svelte
@@ -2,30 +2,37 @@
 	import { navProgress } from "$stores/navProgress";
 	import { onMount } from "svelte";
 
+	let progressTimeout: NodeJS.Timeout;
+
 	const beforepreparation = () => {
+		console.log("beforepreparation");
 		if (progressTimeout) clearTimeout(progressTimeout);
 		$navProgress = 15;
 	};
 	const beforeswap = () => {
+		console.log("beforeswap");
 		$navProgress = 75;
 	};
 	const afterswap = () => {
+		console.log("afterswap");
 		$navProgress = 90;
 	};
 	const pageload = () => {
+		console.log("pageload");
 		$navProgress = 100;
 		if (progressTimeout) clearTimeout(progressTimeout);
 		progressTimeout = setTimeout(() => {
+			console.log("pageload timeout");
 			$navProgress = 0;
 		}, 500);
 	};
 
-	let progressTimeout: NodeJS.Timeout;
 	onMount(() => {
 		document.addEventListener("astro:before-preparation", beforepreparation);
 		document.addEventListener("astro:before-swap", beforeswap);
 		document.addEventListener("astro:after-swap", afterswap);
 		document.addEventListener("astro:page-load", pageload);
+
 		return () => {
 			document.removeEventListener(
 				"astro:before-preparation",
